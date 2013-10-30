3802e65ae544b868dd27699b3d08d85aa3a23e37
Xianjun <sterding@gmail.com>
2013-10-30 16:34:15 -0400

    module template for Shuilin

---
+++ _bam2vcf.sh
@@ -0,0 +1,16 @@
+# Script to discover variations from input BAM file
+# author: xxx
+# Date: 2013-10-30
+# Version: 1.0
+# Usage: bsub _bam2vcf.sh <input.bam>
+
+bamfile=$1
+vcffile=${bamfile/bam/vcf}
+
+
+
+
+
+
+
+
