1e450c51140bea1436a9e6946ef81b4d24c11741
enoxam <lablandsdowne@bwh000948.mgh.harvard.edu>
2013-12-16 14:33:19 -0500

    updated RNA-SeQC module

--- modules/_makeSamplelistfile.sh
+++ modules/_makeSamplelistfile.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-# echo "$PWD"
-## passing parameters 
-# echo $1
-
 ls -lh $1 | awk '{gsub(/\/accepted_hits.bam/,"",$9); print $9}' | awk '{gsub(/\/[\/a-zA-Z0-9\_\/]*\//,"", $1); print $1}' > t1.txt
-
 ls -lh $1 | awk '{print $9}' > t2.txt
-
 wc  t1.txt > tlen.txt
 read lines words characters filename < tlen.txt
-
-# echo "lines=$lines words=$words characters=$characters filename=$filename"
 
 seq $lines | awk '{print "sample"$1}' > t3.txt
 
@@ -21,4 +13,4 @@ echo -e Sample'\t'IDBam'\t'FileNotes > thead.txt
 
 cat thead.txt t4.txt > samplelist_file.txt
 
-rm t1.txt t2.txt t3.txt t4.txt t5.txt tlen.txt
+rm t1.txt t2.txt t3.txt t4.txt t5.txt tlen.txt thead.txt
