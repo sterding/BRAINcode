f5466cc339a381a3f9857d9b74dd45902389f964
enoxam <lablandsdowne@bwh000948.mgh.harvard.edu>
2013-12-30 14:08:46 -0500

    updated _cluster.R

--- modules/_cluster.sh
+++ modules/_cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-Rscript /PHShome/bz016/clustComRNASeq.R "/PHShome/bz016/temp" "/PHShome/bz016"
+Rscript /PHShome/bz016/neurogen/rnaseq_PD/results/SampleClusterResults/clustComRNASeq.R "/PHShome/bz016/neurogen/rnaseq_PD/run_output" "/PHShome/bz016/neurogen/rnaseq_PD/results/SampleClusterResults"
